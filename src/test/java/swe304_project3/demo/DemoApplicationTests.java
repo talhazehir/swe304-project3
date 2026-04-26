cf8be5bfd726c1cf1d1c1c1b00fdbdbe401672c8
talhazehir <mustafa.zehir@ogr.sakaryaedu.tr>
2026-04-26 15:58:37 +0300

    Initial commit of Spring Boot app

---
+++ src/test/java/swe304_project3/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package swe304_project3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
